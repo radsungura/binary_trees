e1ca6645438e90b68ef7f7d05ada7f6b6b1f0288
radsungura <>
2024-03-08 11:46:42 +0200

    fix error

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,13 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree 
- * using post-order traversal.
+ * binary_tree_postorder - Goes through a binary by tree post-order traversal.
  *
- * @tree: pointer to the root node of the tree to traverse.
- * @func: pointer to a function to call for each node.
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node.
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
